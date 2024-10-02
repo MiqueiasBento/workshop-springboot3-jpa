b7779abd550249cc19f7fa3926d0708d3a236c8e
Miqueias Bento <miqueias.bento32@gmail.com>
2024-10-02 10:31:29 -0300

    Order, Instant, ISO 8601 - PART I

---
+++ src/main/java/com/example/course/entities/Order.java
@@ -0,0 +1,78 @@
+package com.example.course.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity		// Anotation para entidade
+@Table(name = "tb_order")	// Define o nome da tabela
+public class Order implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id														// Marca como id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)		// Gera id automatico
+	private Integer id;
+	private Instant date;
+	
+	@ManyToOne		// Informa o JPA que e uma chave estrangeira
+	@JoinColumn(name = "client_id")
+	private User client;
+	
+	public Order() {
+	}
+
+	public Order(Integer id, Instant date, User client) {
+		this.id = id;
+		this.date = date;
+		this.client = client;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
+	}
+}
--- src/main/java/com/example/course/entities/User.java
+++ src/main/java/com/example/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.example.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,9 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String passwaord;
+	
+	@OneToMany(mappedBy = "client")		// Associocao e mapeado na associacao por "client"
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -72,6 +78,10 @@ public class User implements Serializable{
 
 	public void setPasswaord(String passwaord) {
 		this.passwaord = passwaord;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
