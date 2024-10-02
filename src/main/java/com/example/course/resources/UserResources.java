e9928ce5d38f9ae76f3f47496db5c89915cea9a2
Miqueias Bento <miqueias.bento32@gmail.com>
2024-10-01 23:07:22 -0300

    Service layer, component registration

--- src/main/java/com/example/course/resources/UserResources.java
+++ src/main/java/com/example/course/resources/UserResources.java
@@ -1,21 +1,35 @@
 package com.example.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.course.entities.User;
+import com.example.course.services.UserService;
 
 @RestController		//Define que esta classe é um controlador REST.
 @RequestMapping(value = "/users")	//Mapeia a URL base "/users" para esse controlador.
 public class UserResources {
-
+	
+	@Autowired		// Injecao de dependencia
+	private UserService service;
+	
 	@GetMapping		// Mapeia requisições GET para o método abaixo.
-	public ResponseEntity<User> findAll() {
+	public ResponseEntity<List<User>> findAll() {
 		// Método que retorna um objeto User dentro de um ResponseEntity.
-
-		User u = new User(1, "Maria", "maria@gmail.com", "9999-9999", "12345");
-		return ResponseEntity.ok().body(u);		// Retorna uma resposta 200 OK com o usuário no corpo.
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);		// Retorna uma resposta 200 OK com o usuário no corpo.
 	}
+	
+	@GetMapping(value = "/{id}")	// Mapeia o novo caminho de url para o metodo
+	public ResponseEntity<User> findById(@PathVariable Integer id) {	// PathVariable para aceitar o parametro como url
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
---
+++ src/main/java/com/example/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.example.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.course.entities.User;
+import com.example.course.repositories.UserRepository;
+
+@Service	// Registro como componente de servico do Spring, para o uso do Autowired
+public class UserService {
+	
+	@Autowired	// Para injecao de dependencia
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Integer id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
