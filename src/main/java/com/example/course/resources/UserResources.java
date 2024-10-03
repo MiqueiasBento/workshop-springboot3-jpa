a70b35ac8a23a3bb2766eb0a2925af3e87d3e404
Miqueias Bento <miqueias.bento32@gmail.com>
2024-10-03 15:30:36 -0300

    User insert

--- src/main/java/com/example/course/resources/UserResources.java
+++ src/main/java/com/example/course/resources/UserResources.java
@@ -1,13 +1,17 @@
 package com.example.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.course.entities.User;
 import com.example.course.services.UserService;
@@ -32,4 +36,11 @@ public class UserResources {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {	// Recebe o objeto "User" no corpo da requisição
+	    obj = service.insert(obj); // Chama o método de inserção no service, que salva o usuário
+	    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/id")  // Cria a URI para o novo recurso
+	            .buildAndExpand(obj.getId()).toUri(); // Expande o ID do usuário no path da URI
+	    return ResponseEntity.created(uri).body(obj); // Retorna o status "created" com a URI e o objeto salvo
+	}
 }
