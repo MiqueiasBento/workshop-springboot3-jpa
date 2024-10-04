9367739ae676d971aa3828db5a40bdfcf882ba43
Miqueias Bento <miqueias.bento32@gmail.com>
2024-10-03 23:52:03 -0300

    Exception handling - delete

--- src/main/java/com/example/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/course/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.example.course.services.exceptions.DatabaseException;
 import com.example.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -29,6 +30,14 @@ public class ResourceExceptionHandler { // Classe que intercepta exceções em t
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) { // Trata exceções do tipo ResourceNotFoundException
         String error = "Resource not found"; // Define a mensagem de erro
         HttpStatus status = HttpStatus.NOT_FOUND; // Define o status HTTP 404 (Not Found)
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI()); // Cria um objeto StandardError com detalhes do erro
+        return ResponseEntity.status(status).body(err); // Retorna a resposta HTTP com o status e o corpo contendo os detalhes do erro
+    }
+    
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) { // Trata exceções do tipo DatabaseException
+        String error = "Database error"; // Define a mensagem de erro
+        HttpStatus status = HttpStatus.BAD_REQUEST; // Define o status de BAD_REQUEST
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI()); // Cria um objeto StandardError com detalhes do erro
         return ResponseEntity.status(status).body(err); // Retorna a resposta HTTP com o status e o corpo contendo os detalhes do erro
     }
